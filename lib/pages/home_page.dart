1fd6b0a0cca4a6c52b6c712c74a04555a8e8fd04
Junio-Alves <juniophb2004@gmail.com>
2024-10-15 11:47:06 -0300

    ajustes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,65 +1,72 @@
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geolocator/geolocator.dart';
+  import 'dart:async';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  import 'package:flutter/material.dart';
+  import 'package:google_maps_flutter/google_maps_flutter.dart';
+  import 'package:geolocator/geolocator.dart';
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
+  class HomePage extends StatefulWidget {
+    const HomePage({super.key});
 
-class _HomePageState extends State<HomePage> {
-  GoogleMapController? _controller;
-  LatLng initialPosition = const LatLng(-15.793889, -47.882778);
-
-  onMapCreated(GoogleMapController controller) {
-    _controller = controller;
+    @override
+    State<HomePage> createState() => _HomePageState();
   }
 
-  recuperarPosicao() async {
-    //permissao
-    bool serviceEnabled;
-    LocationPermission permission;
+  class _HomePageState extends State<HomePage> {
+    GoogleMapController? _controller;
+    LatLng? userLocation;
+    StreamSubscription<Position>? positionStream;
 
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (serviceEnabled == false) {
-      print("Serviço de localização desabilitado");
+    onMapCreated(GoogleMapController controller) {
+      _controller = controller;
     }
+    /*
+    recuperarPosicao() async {
+    Position position = await Geolocator.getCurrentPosition();
+    setState(() {
+      userLocation = LatLng(position.latitude, position.longitude);
+    });
+    }
+    */
 
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.checkPermission();
-      if (permission == LocationPermission.denied) {
-        print("Permisão de localização negada");
+    adicionarListenerLocalizacao(){
+      LocationSettings locationSettings = const LocationSettings(accuracy: LocationAccuracy.high,distanceFilter: 0,);
+      positionStream = Geolocator.getPositionStream(locationSettings: locationSettings).listen((Position position) { setState(() {
+        userLocation = LatLng(position.latitude, position.longitude,);
+      },);});
+
+      if(_controller != null){
+        _controller!.animateCamera(CameraUpdate.newLatLng(userLocation!),);
       }
     }
 
-    Position position = await Geolocator.getCurrentPosition();
-    initialPosition = LatLng(position.latitude, position.longitude);
-  }
+    @override
+    void initState() {
+      // TODO: implement initState
+      super.initState();
+      adicionarListenerLocalizacao();
+    }
+    @override
+    void dispose() {
+      // TODO: implement dispose
+      super.dispose();
+      positionStream?.cancel();
+    }
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    recuperarPosicao();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Mapas"),
-      ),
-      body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: CameraPosition(
-          target: initialPosition,
-          zoom: 11,
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Mapas"),
         ),
-        onMapCreated: onMapCreated,
-      ),
-    );
+        body: userLocation == null ? const Center(child: CircularProgressIndicator(),): GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: CameraPosition(
+            target: userLocation!,
+            zoom: 15,
+          ),
+          onMapCreated: onMapCreated,
+          myLocationEnabled: true,
+        ),
+      );
+    }
   }
-}
